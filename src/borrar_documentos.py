67bcb80ef9df8cdcc88ca373a61a9b241f64694a
DSastreHernandez <dsastre@cifpfbmoll.eu>
2021-12-03 20:28:31 +0100

    feat: :sparkles: Modulos para borrar
    Modulos para borrar los documentos de la base de datos

---
+++ src/borrar_documentos.py
@@ -0,0 +1,12 @@
+from conectar_con_mongo import conectar_con_mongo
+
+def borrar_documentos (query):
+    try:
+        collection = conectar_con_mongo()
+        encontrado = collection.find_one(query)
+        if len(encontrado) > 0:
+            collection.delete_many(query)
+    except:
+        print('No se han podido encontrar los documentos ' + str(query) + ' que queria eliminar.')
+    else:
+        print('Los documentos ' + str(query) + ' han sido borrados')
