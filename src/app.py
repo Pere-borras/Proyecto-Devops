b3c43ada7a788c07b512db97889a31188217ae5c
Pere-borras <pborrasexposito@cifpfbmoll.eu>
2021-12-03 15:46:06 +0100

    ci: :art: Cambios en el código del módulo app
    Estos cambios vienen a raíz de la implementación de CRUD al programa. Ahora el módulo app se utiliza en el caso de que se quieran obtener documentos de la base de datos y convertirlos a markdown

--- src/app.py
+++ src/app.py
@@ -1,17 +1,19 @@
-from Conectar_py_mongo import conectar_pymongo
 from conversor import conversor
 from recopilador_datos import recopilador_datos
 
-counter = -1
-json_files = conectar_pymongo()
 
-for entrance in json_files:
-    if counter >= len(json_files):
-        break
-    else:
+def app(json_files):
+
+    counter = 0
+
+    if len(json_files) <= 0:
+        print('No se ha encontrado ningún archivo. ¿Ha introducido la query de forma correcta?')
+        return
+
+    for entrance in json_files:
         counter += 1
-    json_arr = json_files[counter]
-    output = recopilador_datos(json_arr)
-    conversor(output)
-print('Todos los archivos han sido convertidos (total de archivos -> ' +
-      str(counter + 1) + ')')
+        json_arr = entrance
+        output = recopilador_datos(json_arr)
+        conversor(output)
+    print('Todos los archivos han sido convertidos (total de archivos -> ' +
+          str(counter) + ')')
