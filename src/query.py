95fb3c6f8d2ff38dd4d5e76e02269f879ea3b781
Pere-borras <pborrasexposito@cifpfbmoll.eu>
2021-12-03 15:44:20 +0100

    feat: :art: Nuevos módulos para realizar consultas
    Módulo query y request_query añadidos que utilizan inputs creados por el usuario (introducidos manualmente por consola). Usado para realizar CRUD en la BD

--- src/query.py
+++ src/query.py
@@ -1,58 +1,13 @@
-from pprint import pprint
+def query(request):
 
+    if request == '':
+        request = '{' + request + '}'
+        return request
 
-def query():
-    schema = {
-        "Nombre pack": "Pack Squanchy Style",
-        "calidad": "Basic",
-        "precio": "7",
-        "stock": "25",
-        "dimensiones": {
-            "altura": "3 cm",
-            "ancho": "6 cm"
-        },
-        "contenidos": {
-            "Rifle de pulso": {
-                "caracteristicas": {
-                    "Precio": "5",
-                    "Calidad": "30",
-                    "Material": "normal",
-                    "Cantidad": "1",
-                    "Stock": "25",
-                    "Demanda": "10"
-                }
-            },
-            "Poción del arcoiris": {
-                "caracteristicas": {
-                    "Precio": "2",
-                    "Calidad": "20",
-                    "Material": "consumible",
-                    "Cantidad": "1",
-                    "Stock": "50",
-                    "Demanda": "63"
-                }
-            }
-        }
-    }
-    print()
-    print('El siguiente diccionario es un ejemplo del esquema a seguir para realizar una búsqueda correcta en la base de datos de MongoDB:')
-    print()
+    if request[0] != '{':
+        request = '{' + request
 
-    pprint(schema)
-    print()
-    print()
+    if request[-1] != '}':
+        request = request + '}'
 
-    query = input(
-        'Indique los campos que quiera buscar con la sintaxis "campo":"valor", recuerde que si quiere consultar campos que están dentro de otros campos, debe ajustarse al esquema. Si quiere buscar todos los documentos de la colección, simplemente pulse Enter: ')
-
-    if query == '':
-        query = '{' + query + '}'
-        return
-
-    if query[0] != '{':
-        query = '{' + query
-
-    if query[-1] != '}':
-        query = query + '}'
-
-    return query
+    return request
