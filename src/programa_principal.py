1c38b44a291b07c8ba598ba916340cda48afdcaf
Pere-borras <pborrasexposito@cifpfbmoll.eu>
2021-12-03 16:09:31 +0100

    style: :zap: Módulo programa principal da más información al usuario
    Ahora si se selecciona una de las opciones que no están disponibles, el programa se lo notificará al usuario

--- src/programa_principal.py
+++ src/programa_principal.py
@@ -8,7 +8,7 @@ print()
 pregunta = input('¡Bienvenido! Si deseas crear documentos en la base de datos, introduce "1". Si deseas encontrar datos en la base de datos, introduce "2". Si deseas actualizar algún campo en la base de datos, introduce "3". Si deseas eliminar algún documento en la base de datos, introduce "4": ')
 
 if pregunta == '1':
-    consulta = query(query_request())
+    print('Actualmente esta función no está disponible')
 
 elif pregunta == '2':
     consulta = query(query_request())
@@ -16,10 +16,10 @@ elif pregunta == '2':
     app(json_files)
 
 elif pregunta == '3':
-    consulta = query(query_request())
+    print('Actualmente esta función no está disponible')
 
 elif pregunta == '4':
-    consulta = query(query_request())
+    print('Actualmente esta función no está disponible')
 
 else:
     print('Ha introducido un dato incorrecto, por favor, introdúzcalo de nuevo')
