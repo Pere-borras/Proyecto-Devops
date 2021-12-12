e2ff23327c3c7cde61df630be8c2f7cce4b66e67
DSastreHernandez <dsastre@cifpfbmoll.eu>
2021-12-12 20:30:26 +0100

    perf: :zap: He perfeccionado el modulo insertar documento
    He eliminado funciones innecesarias mejorando el codigo en insertar documento y recopilar datos

--- src/insertar_documentos/insertar_documento.py
+++ src/insertar_documentos/insertar_documento.py
@@ -1,22 +1,15 @@
-from insertar_documentos.recopilar_datos_documento import recopilar_datos_documento
-from insertar_documentos.insertar_documento_basic import insertar_documento_basic
-from insertar_documentos.insertar_documento_standard import insertar_documento_standard
-from insertar_documentos.insertar_documento_premium import insertar_documento_premium
+from mongo.conectar_con_mongo import conectar_con_mongo
+from recopilar_datos_documento import recopilar_datos_documento
 
 def insertar_documento ():
     try:
-        nombre_pack,calidad_pack,stock_pack,dimensiones_pack,contenido = recopilar_datos_documento()
+        documento = recopilar_datos_documento()
+        
+        colleccion = conectar_con_mongo()
     
-        if calidad_pack.lower() == 'basic':
-            insertar_documento_basic(nombre_pack,stock_pack,dimensiones_pack,contenido)
+        colleccion.insert_one(documento)
+        
+        print("Se ha introducido el documento.")
     
-        elif calidad_pack.lower() == 'standard':
-            insertar_documento_standard(nombre_pack,stock_pack,dimensiones_pack,contenido)
-    
-        elif calidad_pack.lower() == 'premium':
-            insertar_documento_premium(nombre_pack,stock_pack,dimensiones_pack,contenido)
-    
-        else:
-            print('Ha introducido mal la calidad del pack')
     except:
         return  
--- src/insertar_documentos/recopilar_datos_documento.py
+++ src/insertar_documentos/recopilar_datos_documento.py
@@ -1,51 +1,113 @@
 def recopilar_datos_documento():
     
-    nombre_pack = input('Escriba el nombre del pack...\n')
+    nombre_pack = {"Nombre pack":input('Escriba el nombre del pack...\n')}
     
-    calidad_pack = input('Escriba la calidad del pack, eligiendo entre: Basic, Standard y Premium\n')
+    calidad_pack = {"calidad":input('Escriba la calidad del pack, eligiendo entre: Basic, Standard y Premium\n')}
+
+    stock_pack = {"stock":input('Introduzca el stock del pack\n')}
     
-    stock_pack = input('Introduzca el stock del pack\n')
-    
-    altura_pack = input('Introduzca la altura en centimetros\n')
-    
-    ancho_pack = input('Introduzca el ancho en cm\n')
-    
-    dimensiones_pack = [altura_pack, ancho_pack]
-    
-    cantidad_contenidos = {'basic':2,'standard':3,'premium':4}
+    dimensiones_pack = {"dimensiones":{
+        "altura":input("Introduzca la altura del pack en cm\n"),
+        "ancho":input("Introduzca el ancho del pack en cm\n")
+    }}
     
     contenido = {}
-    contador = 0
-    if calidad_pack in cantidad_contenidos.keys():
-        # Al entrar al for se decide las vueltas que dara dependiendo de la calidad del pack. Cada vuelta es un contenido
-        for number in range(0,cantidad_contenidos[calidad_pack.lower()]):
+    if calidad_pack["calidad"] == "Basic":
+        contenido.update({"contenidos":
+        {input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}},
+        input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}}
+             }})
+    elif calidad_pack["calidad"] == "Standard":
+        contenido.update({"contenidos":
+        {input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}},
+        input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}},
+        input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}}
+             }})
+    elif calidad_pack["calidad"] == "Premium":
+        contenido.update({"contenidos":
+        {input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}},
+        input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}},
+        input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}},
+        input("Introduzca el nombre del contenido\n"):{"caracteristicas":
+            {"Precio":input("Introduzca el precio del contenido\n"),
+             "Calidad":input("Introduzca la calidad del contenido\n"),
+             "Material":input("Introduzca el material del que esta hecho\n"),
+             "Cantidad":input("Introduzca la cantidad de veces que viene este contenido en el pack\n"),
+             "Stock":input("Introduzca el stock del contenido\n"),
+             "Demanda":input("Introduzca la demanda del contenido\n")}}
+             }})
     
-            contenido[contador]= input('Escriba el nombre del contenido\n')
-            contador += 1
-
-            # Aqui se hace un try except para que si o si ese contenido sea un numero ya que el precio del pack es la suma del precio de los contenidos
-            try:
-                contenido[contador]= float(input('Introduzca el precio del contenido\n'))
-                contador += 1
-            except:
-                print('Por favor introduzca solo numeros\nIntentelo de nuevo')
-                return
-    
-            contenido[contador]= input('Escriba la calidad del contenido\n')
-            contador += 1
-    
-            contenido[contador]= input('Escriba el material del contenido\n')
-            contador += 1
-    
-            contenido[contador]= input('Introduzca la cantidad del contenido \n')
-            contador += 1
-    
-            contenido[contador]= input('Introduzca el stock del contenido\n')
-            contador += 1
-    
-            contenido[contador]= input('Introduzca la demanda del contenido\n')
-            contador += 1
-    
-        return nombre_pack,calidad_pack,stock_pack,dimensiones_pack,contenido
     else:
-        print('Se ha producido un error, por favor intentelo de nuevo.')
+        print("Ha introducido las la clase, Por favor intentelo de nuevo")
+    
+    try:
+        suma_precio_pack = 0
+        valores_contenido = contenido.values()
+            
+        for s in valores_contenido:
+            for x in s:
+                caracteristicas = s[x]
+                suma_precio_pack += int(caracteristicas["caracteristicas"]["Precio"])
+        precio_pack = {"precio":str(suma_precio_pack)}
+    except:
+            print("Ha introducido mal el precio de los contenidos\n\
+                Por favor intentelo de nuevo")
+    else:
+            documento = {}
+            documento.update(nombre_pack)
+            documento.update(calidad_pack)
+            documento.update(precio_pack)
+            documento.update(stock_pack)
+            documento.update(dimensiones_pack)
+            documento.update(contenido)
+            return documento
+
+recopilar_datos_documento()
