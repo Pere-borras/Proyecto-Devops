e42c5a0b09666cfa95d280a2df7fa0909a8a5107
Pere-borras <pborrasexposito@cifpfbmoll.eu>
2021-12-12 19:36:21 +0100

    fix: :bug: Reparación de bugs
    No se eliminaba según lo esperado

--- src/eliminar_documentos/borrar_documentos.py
+++ src/eliminar_documentos/borrar_documentos.py
@@ -1,10 +1,16 @@
-from mongo.conectar_con_mongo import conectar_con_mongo
+from mongo_archivos.conectar_con_mongo import conectar_con_mongo
 
 def borrar_documentos (query):
     try:
+        archivos = []
+
         collection = conectar_con_mongo()
-        encontrado = collection.find_one(query)
-        if len(encontrado) > 0:
+        encontrado = collection.find(query)
+
+        for archivo in encontrado:
+            archivos.append(archivo)
+
+        if len(archivos) > 0:
             collection.delete_many(query)
     except:
         print('No se han podido encontrar los documentos ' + str(query) + ' que queria eliminar.')
